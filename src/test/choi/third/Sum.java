5409d877d9168d5cabd0f2b7b601cae72d856397
asus <asus@DESKTOP-MPDMUUP>
2017-09-12 21:29:02 +0900

    get input as String

--- src/test/choi/third/Sum.java
+++ src/test/choi/third/Sum.java
@@ -14,12 +14,17 @@ public class Sum {
 	 */
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int[] number = new int[2];
+		String[] number = new String[2];
+		int[] get_number = new int[2];
 		System.out.println("두 수를 입력하세요.");
-		number[0] = sc.nextInt();
-		number[1] = sc.nextInt();
+		number[0] = sc.next();
+		number[1] = sc.next();
+		get_number[0] = Integer.parseInt(number[0]);
+		get_number[1] = Integer.parseInt(number[1]);
 		
-		System.out.println("두 수의 합은 " + (number[0] + number[1]) + " 입니다.");
+		System.out.println("두 수의 합은 " + (get_number[0] + get_number[1]) + " 입니다.");
+		
+		sc.close();
 	}
 
 }
